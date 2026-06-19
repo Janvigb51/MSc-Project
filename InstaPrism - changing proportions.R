260db3a8fca5c974b528374fc720b24593fd7bca
Janvi G <janvigb51@gmail.com>
2026-06-19 12:32:41 +0100

    Changing proportions of each CAF in lung data

--- InstaPrism - changing proportions.R
+++ InstaPrism - changing proportions.R
@@ -11,7 +11,7 @@ base_props <- base_props[celltypes]
 base_props
 
 # 3) Varying proportions with CUSTOM
-target_cell <- "IDO_CAF"
+target_cell <- "vCAF"
 target_props <- seq(0.02, 0.50, by = 0.04)
 replicates <- 10
 custom_rows <- list()
@@ -38,8 +38,8 @@ for (p in target_props) {
 custom_scenario_data <- as.data.frame(do.call(rbind, custom_rows))
 
 # 4) Simulation
-simulation_IDO_CAF_custom <- SimBu::simulate_bulk(
-  data = cords_breast,
+simulation_vCAF_custom <- SimBu::simulate_bulk(
+  data = cords_lung,
   scenario = "custom",
   custom_scenario_data = custom_scenario_data,
   ncells = 2000,
@@ -49,50 +49,50 @@ simulation_IDO_CAF_custom <- SimBu::simulate_bulk(
 )
 
 # 5) Check Results
-bulk_expr_IDO_CAF <- SummarizedExperiment::assays(simulation_IDO_CAF_custom$bulk)[["bulk_counts"]]
-dim(bulk_expr_IDO_CAF)
-dim(simulation_IDO_CAF_custom$cell_fractions)
-head(simulation_IDO_CAF_custom$cell_fractions)
+bulk_expr_vCAF <- SummarizedExperiment::assays(simulation_vCAF_custom$bulk)[["bulk_counts"]]
+dim(bulk_expr_vCAF)
+dim(simulation_vCAF_custom$cell_fractions)
+head(simulation_vCAF_custom$cell_fractions)
 
 # 6) Deconvolution
-bulk_expr_IDO_CAF <- as.matrix(bulk_expr_IDO_CAF)
-deconv_IDO_CAF_custom <- InstaPrism(bulk_Expr = bulk_expr_IDO_CAF, refPhi_cs = refPhi_obj)
+bulk_expr_vCAF <- as.matrix(bulk_expr_vCAF)
+deconv_vCAF_custom <- InstaPrism(bulk_Expr = bulk_expr_vCAF, refPhi_cs = refPhi_obj)
 
 # Predicted cell type proportions
-estimated_IDO_CAF_custom <- t(deconv_IDO_CAF_custom@Post.ini.ct@theta)
-head(estimated_IDO_CAF_custom)
+estimated_vCAF_custom <- t(deconv_vCAF_custom@Post.ini.ct@theta)
+head(estimated_vCAF_custom)
 
 # Ground truth cell type proportions
-truth_IDO_CAF_custom <- simulation_IDO_CAF_custom$cell_fractions
-truth_IDO_CAF_custom <- truth_IDO_CAF_custom[
-  rownames(estimated_IDO_CAF_custom),
-  colnames(estimated_IDO_CAF_custom)]
+truth_vCAF_custom <- simulation_vCAF_custom$cell_fractions
+truth_vCAF_custom <- truth_vCAF_custom[
+  rownames(estimated_vCAF_custom),
+  colnames(estimated_vCAF_custom)]
 
 # 7) Plot results
 plot(
-  truth_IDO_CAF_custom[, "IDO_CAF"],
-  estimated_IDO_CAF_custom[, "IDO_CAF"],
-  xlab = "True IDO_CAF proportion",
-  ylab = "Estimated IDO_CAF proportion",
-  main = "Deconvolution performance with varying IDO_CAF",
+  truth_vCAF_custom[, "vCAF"],
+  estimated_vCAF_custom[, "vCAF"],
+  xlab = "True vCAF proportion",
+  ylab = "Estimated vCAF proportion",
+  main = "Deconvolution performance with varying vCAF",
   pch = 16)
 
 abline(0, 1, col = "red", lty = 2, lwd = 2)
 
 # 8) Performance Evaluation
 # Pearson Correlation
-IDO_CAF_cor <- cor(truth_IDO_CAF_custom[, "IDO_CAF"],estimated_IDO_CAF_custom[, "IDO_CAF"])
-IDO_CAF_cor
+vCAF_cor <- cor(truth_vCAF_custom[, "vCAF"],estimated_vCAF_custom[, "vCAF"])
+vCAF_cor
 # RMSE
-IDO_CAF_rmse <- sqrt(mean((truth_IDO_CAF_custom[, "IDO_CAF"] - estimated_IDO_CAF_custom[, "IDO_CAF"])^2))
-IDO_CAF_rmse
+vCAF_rmse <- sqrt(mean((truth_vCAF_custom[, "vCAF"] - estimated_vCAF_custom[, "vCAF"])^2))
+vCAF_rmse
 # Add metrics to plot
 text(
   x = 0.045,
   y = 0.52,
   labels = paste0(
-    "Pearson Correlation = ", round(IDO_CAF_cor, 4),
-    "\nRMSE = ", round(IDO_CAF_rmse, 4)
+    "Pearson Correlation = ", round(vCAF_cor, 4),
+    "\nRMSE = ", round(vCAF_rmse, 4)
   ),
   adj = 0
 )
@@ -100,68 +100,72 @@ text(
 # 9) Faceted plots
 
 ## Option 1: Estimated proportion of other CAFs ################################
-plot_IDO_CAF_df <- data.frame(
-  sample = rep(rownames(truth_IDO_CAF_custom), times = ncol(truth_IDO_CAF_custom)),
-  CAFtype = rep(colnames(truth_IDO_CAF_custom), each = nrow(truth_IDO_CAF_custom)),
+plot_vCAF_df <- data.frame(
+  sample = rep(rownames(truth_vCAF_custom), times = ncol(truth_vCAF_custom)),
+  CAFtype = rep(colnames(truth_vCAF_custom), each = nrow(truth_vCAF_custom)),
   
-  # x-axis: the manipulated IDO_CAF truth, repeated for every CAF panel
-  true_IDO_CAF = rep(truth_IDO_CAF_custom[, "IDO_CAF"], times = ncol(truth_IDO_CAF_custom)),
+  # x-axis: the manipulated vCAF truth, repeated for every CAF panel
+  true_vCAF = rep(truth_vCAF_custom[, "vCAF"], times = ncol(truth_vCAF_custom)),
   
   # y-axis: estimated proportion for each CAF type
-  estimated = as.vector(as.matrix(estimated_IDO_CAF_custom)),
+  estimated = as.vector(as.matrix(estimated_vCAF_custom)),
   
   # optional: the actual true proportion of each CAF type in that sample
-  true_CAF = as.vector(as.matrix(truth_IDO_CAF_custom)))
+  true_CAF = as.vector(as.matrix(truth_vCAF_custom)))
 
 library(ggplot2)
-ggplot(plot_IDO_CAF_df, aes(x = true_IDO_CAF, y = estimated)) +
+ggplot(plot_vCAF_df, aes(x = true_vCAF, y = estimated)) +
   geom_point(size = 1.8, alpha = 0.7) +
   facet_wrap(~ CAFtype, ncol = 5) +
   theme_bw() +
   labs(
-    title = "Effect of changing IDO_CAF on other CAFs",
-    x = "True IDO_CAF proportion",
+    title = "Effect of changing vCAF on other CAFs",
+    x = "True vCAF proportion",
     y = "Estimated CAF proportion")
 
 ## Option 2: True + Estimated proportion of other CAFs #########################
 
-plot_IDO_CAF_compare_df <- rbind(
+plot_vCAF_compare_df <- rbind(
   data.frame(
-    sample = rep(rownames(truth_IDO_CAF_custom), times = ncol(truth_IDO_CAF_custom)),
-    CAFtype = rep(colnames(truth_IDO_CAF_custom), each = nrow(truth_IDO_CAF_custom)),
-    true_IDO_CAF = rep(truth_IDO_CAF_custom[, "IDO_CAF"], times = ncol(truth_IDO_CAF_custom)),
-    value = as.vector(as.matrix(truth_IDO_CAF_custom)),
+    sample = rep(rownames(truth_vCAF_custom), times = ncol(truth_vCAF_custom)),
+    CAFtype = rep(colnames(truth_vCAF_custom), each = nrow(truth_vCAF_custom)),
+    true_vCAF = rep(truth_vCAF_custom[, "vCAF"], times = ncol(truth_vCAF_custom)),
+    value = as.vector(as.matrix(truth_vCAF_custom)),
     Source = "Truth"
   ),
   data.frame(
-    sample = rep(rownames(estimated_IDO_CAF_custom), times = ncol(estimated_IDO_CAF_custom)),
-    CAFtype = rep(colnames(estimated_IDO_CAF_custom), each = nrow(estimated_IDO_CAF_custom)),
-    true_IDO_CAF = rep(truth_IDO_CAF_custom[, "IDO_CAF"], times = ncol(estimated_IDO_CAF_custom)),
-    value = as.vector(as.matrix(estimated_IDO_CAF_custom)),
+    sample = rep(rownames(estimated_vCAF_custom), times = ncol(estimated_vCAF_custom)),
+    CAFtype = rep(colnames(estimated_vCAF_custom), each = nrow(estimated_vCAF_custom)),
+    true_vCAF = rep(truth_vCAF_custom[, "vCAF"], times = ncol(estimated_vCAF_custom)),
+    value = as.vector(as.matrix(estimated_vCAF_custom)),
     Source = "InstaPrism"
   )
 )
 
-ggplot(plot_IDO_CAF_compare_df, aes(x = true_IDO_CAF, y = value, colour = Source)) +
+ggplot(plot_vCAF_compare_df, aes(x = true_vCAF, y = value, colour = Source)) +
   geom_point(size = 1.6, alpha = 0.7) +
   stat_summary(aes(group = Source), fun = mean, geom = "line", linewidth = 0.9) +
-  facet_wrap(~ CAFtype, ncol = 5) +
+  facet_wrap(~ CAFtype, ncol = 4) +
   theme_bw() +
+  theme(
+    legend.position = c(0.82, 0.32),
+    legend.justification = c(0, 1),
+    legend.background = element_blank()) +
   labs(
-    title = "True vs estimated CAFs when IDO_CAF is varied",
-    x = "True IDO_CAF proportion",
+    title = "True vs estimated CAFs when vCAF is varied",
+    x = "True vCAF proportion",
     y = "CAF proportion"
   )
 
 ############ Full plot #########################################################
 
-# highest_IDO_CAF <- max(truth_IDO_CAF_custom[, "IDO_CAF"])
-# keep_samples <- rownames(truth_IDO_CAF_custom)[
-#   truth_IDO_CAF_custom[, "IDO_CAF"] == highest_IDO_CAF]
+# highest_vCAF <- max(truth_vCAF_custom[, "vCAF"])
+# keep_samples <- rownames(truth_vCAF_custom)[
+#   truth_vCAF_custom[, "vCAF"] == highest_vCAF]
 # 
 # # Full plot dataframe (all CAF types together)
-# truth_high <- truth_IDO_CAF_custom[keep_samples, , drop = FALSE]
-# estimated_high <- estimated_IDO_CAF_custom[keep_samples, , drop = FALSE]
+# truth_high <- truth_vCAF_custom[keep_samples, , drop = FALSE]
+# estimated_high <- estimated_vCAF_custom[keep_samples, , drop = FALSE]
 # 
 # plot_df_high <- data.frame(
 #   truth = as.vector(as.matrix(truth_high)),
@@ -179,8 +183,8 @@ ggplot(plot_IDO_CAF_compare_df, aes(x = true_IDO_CAF, y = value, colour = Source
 #   xlab = "True cell-type proportions",
 #   ylab = "Estimated cell-type proportions",
 #   main = paste0(
-#     "InstaPrism deconvolution performance\n(highest IDO_CAF setting = ",
-#     highest_IDO_CAF, ")"
+#     "InstaPrism deconvolution performance\n(highest vCAF setting = ",
+#     highest_vCAF, ")"
 #   ),
 #   pch = 16,
 #   col = as.factor(plot_df_high$CAFtype)
